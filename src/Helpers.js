8364c174ac71f1d485f4554c4ffec4f8b2fba21c
0xLucasd <>
2023-09-13 10:07:53 +0300

    cooldown period updated

--- src/Helpers.js
+++ src/Helpers.js
@@ -83,7 +83,7 @@ export const MARGIN_FEE_BASIS_POINTS = 10;
 
 export const LIQUIDATION_FEE = expandDecimals(10, USD_DECIMALS);
 
-export const QLP_COOLDOWN_DURATION = 60;
+export const QLP_COOLDOWN_DURATION = 0;
 export const THRESHOLD_REDEMPTION_VALUE = expandDecimals(993, 27); // 0.993
 export const FUNDING_RATE_PRECISION = 1000000;
 
